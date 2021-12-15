8f8627d2952fb0ae320b86ff5e90cdd270d0f756
Nzaro22 <nzaro4@gmail.com>
2021-12-15 20:43:18 +0200

    challenge

--- react_js_14_12_2021/src/App.jsx
+++ react_js_14_12_2021/src/App.jsx
@@ -2,7 +2,7 @@ import './App.css';
 import Name from './components/Name';
 import Register from './components/Register';
 import ThreeCounters from './components/ThreeCounters';
-import VacationList from './components/VacationList/VacationList';
+import VacationList from './components/VacationList';
 import YourName from './components/YourName';
 
 function App() {
